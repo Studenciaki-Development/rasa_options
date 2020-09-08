799f3e7b4a2e110c82ead7bed9614609839b0c5a
Bartek <bartlomiejnawoj@gmail.com>
2020-09-08 10:36:40 +0200

    refactor: change functions and variable names to meet PEP8 standards. - add more specific exceptions when catching errors

--- actions/university/verify_limits.py
+++ actions/university/verify_limits.py
@@ -1,25 +1,25 @@
-def isOnlyStationary(limit_niestat1, limit_niestat2):
+def is_only_stationary(limit_niestat1, limit_niestat2):
     if limit_niestat1 is None and limit_niestat2 is None:
         return True
     else:
         return False
 
 
-def isOnlyFirstDegree(limit_stat2, limit_niestat2):
+def is_only_first_degree(limit_stat2, limit_niestat2):
     if limit_stat2 is None and limit_niestat2 is None:
         return True
     else:
         return False
 
 
-def isOnlySecondtDegree(limit_stat1, limit_niestat1):
+def is_only_second_degree(limit_stat1, limit_niestat1):
     if limit_stat1 is None and limit_niestat1 is None:
         return True
     else:
         return False
 
 
-def isOnlyNonStationary(limit_stat1, limit_stat2):
+def is_only_non_stationary(limit_stat1, limit_stat2):
     if limit_stat1 is None and limit_stat2 is None:
         return True
     else:
@@ -27,11 +27,11 @@ def isOnlyNonStationary(limit_stat1, limit_stat2):
 
 
 def verify_limits(limits):
-    if isOnlyStationary(limits["limit_niestat1"], limits["limit_niestat2"]):
+    if is_only_stationary(limits["limit_niestat1"], limits["limit_niestat2"]):
         return ["field-of-study", "course-level"]
-    elif isOnlyFirstDegree(limits["limit_stat2"], limits["limit_niestat2"]):
+    elif is_only_first_degree(limits["limit_stat2"], limits["limit_niestat2"]):
         return ["field-of-study", "course-type"]
-    elif isOnlyNonStationary(limits["limit_stat1"], limits["limit_stat2"]):
+    elif is_only_non_stationary(limits["limit_stat1"], limits["limit_stat2"]):
         return ["field-of-study", "course-level"]
     else:
         return ["field-of-study", "course-level", "course-type"]
